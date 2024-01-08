f62e9a800184850571b97d81407a18ed5f1c3cf7
Kajetan Wojciechowski <kkajetanwojciechowski@gmail.com>
2024-01-08 02:50:06 +0100

    repost datasets

--- main.py
+++ main.py
@@ -12,15 +12,6 @@ from dataset_loader import get_filenames
 from models_creator import initialize_regression_models, test_regression_models
 
 DATASET_DIR: str = "datasets"
-
-stats = {
-    'model_idx': [],
-    'data_name': [],
-    'data_idx': [],
-    'fold_idx': [],
-    'r2': []
-}
-
 files = get_filenames(DATASET_DIR)
 
 
@@ -32,13 +23,12 @@ def test():
     n_repeats = 1  # Liczba powtórzeń
     random_state = 0  # Ziarno losowości
 
-    # Pętla po modelach
     for model_idx, model in enumerate(regression_models):
-        # print(f"Training {model}...")
+        print(f"Training {model}...")
         for data_name, data_list in balanced_dict.items():
             print(f"Training on {data_name}...")
             for data_idx, data in enumerate(data_list):
-                # print(f"Training on {files[data_idx]}...")
+                print(f"Training on {files[data_idx]}...")
                 ready_data = data
                 weights = np.array([])
                 text_columns = ready_data.select_dtypes(include=['object']).columns.tolist()
@@ -69,9 +59,6 @@ def test():
                     mae_list.append(mean_absolute_error(y_test, y_pred))
                     rmse_list.append(root_mean_squared_error(y_test, y_pred))
 
-                    # dump(model,
-                    #      os.path.join("models", f'model_{model_idx}_{data_name}_{data_idx}_fold_{fold_idx}.joblib'))
-
                 print(sum(mae_list) / len(mae_list))
                 print(sum(rmse_list) / len(rmse_list))
 
@@ -80,11 +67,10 @@ def main():
     balanced_dict = create_balanced_datasets(DATASET_DIR)
     regression_models = initialize_regression_models()
 
-    n_splits = 5  # Liczba podziałów
-    n_repeats = 3  # Liczba powtórzeń
-    random_state = 42  # Ziarno losowości
+    n_splits = 5
+    n_repeats = 3
+    random_state = 13
 
-    # Pętla po modelach
     for model_idx, model in enumerate(regression_models):
         print(f"Training {model}...")
         for data_name, data_list in balanced_dict.items():
@@ -112,15 +98,13 @@ def main():
                     X_train, X_test = ready_data.iloc[train_index], ready_data.iloc[test_index]
                     y_train, y_test = ready_data.iloc[:, -1].iloc[train_index], ready_data.iloc[:, -1].iloc[test_index]
                     if np.any(weights):
-                        model.fit(X_train, y_train, weights[train_index])
+                        train_weights = weights[train_index]
+                        model.fit(X_train, y_train, train_weights)
                     else:
                         model.fit(X_train, y_train)
                     y_pred = model.predict(X_test)
 
                     mae_list.append(mean_absolute_error(y_test, y_pred))
-                    mae_list.append(mean_absolute_error(y_test, y_pred))
-
-                    rmse_list.append(root_mean_squared_error(y_test, y_pred))
                     rmse_list.append(root_mean_squared_error(y_test, y_pred))
 
                     dump(model,
